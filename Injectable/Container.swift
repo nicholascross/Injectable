c39fe97832d4c6a6e508432fc512bb2bfe08745a
Nicholas Cross <nicholas@nacrossweb.com>
2019-01-22 22:07:13 +1100

    Updated documentation comments

--- Injectable/Container.swift
+++ Injectable/Container.swift
@@ -9,8 +9,19 @@
 import Foundation
 
 public protocol Container {
+    //Resolve any concrete type conforming to Injectable
+    //Useful for creating objects with out any registration
     func resolve<Object: Injectable>(lifetime: Lifetime) -> Object
+    
+    //Resolve any concrete type conforming to CustomInjectable
+    //If a resolver has been registered then that will be used instead
+    //to provide additional parameters for object initialisation
+    //Useful for creating varients of the same type
     func resolve<Object: CustomInjectable>(key: String, lifetime: Lifetime) -> Object
+    
+    //Resolve any type so long as it has a registered interface resolver
+    //Useful for creating objects that conform to an interface when
+    //the concrete type is not available at the call site
     func resolveInterface<Interface>() -> Interface!
 }
 
